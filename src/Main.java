9dc4c51df0254bb5a7558ca1dd31c8b5ecd29bd4
jyoung7988 <jyoung7988@naver.com>
2023-05-06 15:11:59 +0900

    [PRG] 해시 - 폰켓몬

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import hash.Solution;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Solution solution = new Solution();
+
     }
 }
