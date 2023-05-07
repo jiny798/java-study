7866f76f6075d351c05d27ed5181ac2b01c76c81
jyoung7988 <jyoung7988@naver.com>
2023-05-07 22:36:09 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import PRG.DFS_BFS.Solution;
+import PRG.DFS_BFS.Solution2;
+import PRG.DFS_BFS.Solution3;
 
 public class Main {
     public static void main(String[] args) {
 
-        Solution solution = new Solution();
-        int[] nums = {1,1,1,1,1};
-        System.out.println(solution.solution(nums,3));
+        Solution3 solution = new Solution3();
+        int[][] maps = { {1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
 
+        System.out.println(solution.solution(maps));
 
     }
 }
