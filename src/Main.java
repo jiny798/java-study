0d2da575313f72ecd1731fa59fe3eefa12d83c14
jyoung7988 <jyoung7988@naver.com>
2023-05-08 22:28:03 +0900

    [PRG] DFS/BFS - 여행경로

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,13 @@ public class Main {
         Solution5 solution5 = new Solution5();
         String[][] arr = {{"ICN","JFK"},{"HND", "IAD"},{"JFK", "HND"}};
 
-        solution5.solution(arr);
+        String[][] arr2 ={{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
+        System.out.println();
+        String[] Ar= solution5.solution(arr2);
+
+//        for(String str : Ar){
+//            System.out.println(str);
+//        }
 
     }
 }
--- src/PRG/DFS_BFS/Solution5.java
+++ src/PRG/DFS_BFS/Solution5.java
@@ -1,28 +1,58 @@
 package PRG.DFS_BFS;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Solution5 {
+    public static int cnt=0;
+    public static Map<String,Integer> map = new HashMap<>();
+    public static boolean tag = false;
     public String[] solution(String[][] tickets) {
 
         String[] airPort = new String[tickets.length+1];
         Arrays.sort(tickets,((o1, o2) ->{
-            return o1[0].compareTo(o2[0]);
+            return o1[1].compareTo(o2[1]);
         } ));
-        for(int i=0;i<tickets.length;i++){
-            airPort[i] = tickets[i][0];  // "ICN", "JFK", "HND",
-        }
         boolean[] checked = new boolean[airPort.length];
+        List<String> list = new ArrayList<>();
+        Stack<String> st = new Stack<>();
 
-        dfs();
+        for(int i=0;i<tickets.length;i++){
+            if(tickets[i][0].equals("ICN")){
+                checked[i]=true;
+                st.add("ICN");
+                dfs(tickets[i][1],tickets,checked,1,st);
+                if(tag){
+                    break;
+                }
+                st.pop();
+                checked[i]=false;
+            }
+        }
 
-
-        return null;
+        Iterator it = st.iterator();
+        int i=0;
+        while (it.hasNext()){
+            airPort[i++] = (String) it.next();
+        }
+        return airPort;
     }
-    public void dfs(int i,String[][] tickets,boolean[] checked){
-
+    public void dfs(String port,String[][] tickets,boolean[] checked,int count,Stack<String> st){
+        if(count == tickets.length){
+            st.add(port); //마지막 공항
+            tag=true;
+            return;
+        }
+        for(int i=0;i<tickets.length;i++){
+            if(tickets[i][0].equals(port) && checked[i]==false ){
+                checked[i]=true;
+                st.add(port);
+                dfs(tickets[i][1],tickets,checked,count+1,st);
+                if(tag){
+                    break;
+                }
+                st.pop();
+                checked[i]=false;
+            }
+        }
     }
 }
