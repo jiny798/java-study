ecf268b9751bd9c5a41615052730e6aa6f8848b7
jiny798 <jyoung7988@naver.com>
2023-06-15 19:54:05 +0900

    commit

---
+++ src/PRG/DP/N으로표현.java
@@ -0,0 +1,9 @@
+package PRG.DP;
+
+public class N으로표현 {
+    public int solution(int N, int number) {
+        //DP[1] -> 1개로 만들수 있는 수
+        //DP[2] -> DP[1] + DP[1] , DP[1] - DP[1], DP[1] * DP[1] ...
+        //DP[3] -> DP[1]
+    }
+}
