280452a6944b0bf163ec6cba1c2dfcc1153f072c
jyoung7988 <jyoung7988@naver.com>
2023-07-05 15:03:19 +0900

    [BOJ] 문자열 폭발

--- src/BOJ/String/문자열_폭발.java
+++ src/BOJ/String/문자열_폭발.java
@@ -3,26 +3,51 @@ package BOJ.String;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class 문자열_폭발 {
+public class 문자열_폭발 { // 스택 사용
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer str = new StringBuffer(br.readLine());
-        String bomb = br.readLine();
-        int index;
-        while (str.indexOf(bomb) != -1) { // 1234 에 12가 있다는 뜻
-            index = str.indexOf(bomb);
-            str.replace(index,index+bomb.length(),"");
-            if(str.length() ==0){
-                break;
+        StringBuffer str = new StringBuffer(br.readLine()); // 12ab112ab
+        String bomb = br.readLine(); // 12ab
+
+        int bombSize = bomb.length();
+
+        char lastStr = bomb.charAt(bomb.length() - 1);
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char s = str.charAt(i); // 1
+
+            stack.add(s);
+            if (s == lastStr && stack.size() >= bombSize) {
+                int stackSize = stack.size(); //
+                String tempStr = "";
+                stackSize -= 1;
+                for (int j = 0; j < bombSize; j++) {
+                    tempStr = stack.get(stackSize--) + tempStr;
+                }
+
+                if (tempStr.equals(bomb)) {
+                    for (int j = 0; j < bombSize; j++) {
+                        stack.pop();
+                    }
+                }
             }
+        }// for end
+
+        StringBuilder sb = new StringBuilder();
+
+        int s = stack.size();
+        for (int j = 0; j < s; j++) {
+            sb.append( stack.pop()) ;
         }
 
-        if(str.length()==0){
-            str.append("FRULA");
+        if (s == 0) {
+            System.out.println("FRULA");
+        } else {
+            System.out.println(sb.reverse());
         }
-
-        System.out.println(str);
 
     }
 }
