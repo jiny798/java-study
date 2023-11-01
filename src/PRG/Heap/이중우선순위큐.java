00ddf5e92d60c4343b5fb85dee8639a372a8c61a
jyoung7988 <jyoung7988@naver.com>
2023-11-01 17:45:22 +0900

    [PRG] 이중우선순위큐

--- src/PRG/Heap/이중우선순위큐.java
+++ src/PRG/Heap/이중우선순위큐.java
@@ -1,30 +1,31 @@
 package PRG.Heap;
+
 import java.util.*;
 
 class 이중우선순위큐 {
-	public static PriorityQueue<Integer> maxHeap ;
-	public static PriorityQueue<Integer> minHeap ;
+	public static PriorityQueue<Integer> maxHeap;
+	public static PriorityQueue<Integer> minHeap;
 
 	public int[] solution(String[] operations) {
 		int[] answer = new int[2];
 		minHeap = new PriorityQueue<>();
 		maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-		for(String str : operations){
-			if(str.substring(0,1).equals("I")){
+		for (String str : operations) {
+			if (str.substring(0, 1).equals("I")) {
 
 				String[] addStr = str.split(" ");
 				minHeap.add(Integer.parseInt(addStr[1]));
 				maxHeap.add(Integer.parseInt(addStr[1]));
 
-			}else if(str.substring(0,3).equals("D 1")){
-				if(maxHeap.size() == 0){
+			} else if (str.substring(0, 3).equals("D 1")) {
+				if (maxHeap.size() == 0) {
 					continue;
 				}
 				int n = maxHeap.poll();
 				minHeap.remove(n);
-			}else{
-				if(minHeap.size() == 0){
+			} else {
+				if (minHeap.size() == 0) {
 					continue;
 				}
 				int n = minHeap.poll();
@@ -32,8 +33,8 @@ class 이중우선순위큐 {
 			}
 		}
 
-		if(maxHeap.size() == 0){
-			answer[0] = 0 ;
+		if (maxHeap.size() == 0) {
+			answer[0] = 0;
 			answer[1] = 0;
 			return answer;
 		}
